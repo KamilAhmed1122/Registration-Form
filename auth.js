40eca6a502986b30d1b78937b4743ddd31047d5e
kamilahmed10121 <166241525+kamilahmed10121@users.noreply.github.com>
2025-01-30 17:42:16 +0500

    main

--- auth.js
+++ auth.js
@@ -1,44 +1,28 @@
-let signupEmail = document.getElementById('signup-email')
-let signupPass = document.getElementById('signup-password')
-let signupBtn = document.getElementById('signup-btn')
-let firstName = document.getElementById("firstName");
-let lastName = document.getElementById("lastName");
-let signupBtnLoader = document.getElementById('loading_btn_spinner')
-let loginBtn = document.getElementById('login_btn')
-let loginEmail = document.getElementById('login-email')
-let loginPass = document.getElementById('login-password')
-let sessionBtn = document.getElementById('session-btn')
-let logoutBtn = document.getElementById('logout-btn')
+let signupEmail = document.getElementById("signup_email");
+let signupPass = document.getElementById("signup_password");
+let signupBtn = document.getElementById("signup-btn");
+let signupBtnLoader = document.getElementById("loading_btn_spinner");
+let loginBtn = document.getElementById("login_btn");
+let loginEmail = document.getElementById("login_email");
+let loginPass = document.getElementById("login_password");
+let sessionBtn = document.getElementById("session_btn");
+let logoutBtn = document.getElementById("logout_btn");
 
-async function signup(){
+let signupName = document.getElementById("signup_name");
+
+async function signup() {
     try {
-        signupBtnLoader.style.display = 'block'
-        const {data, error} = await supabase.auth.signup({
-            email: signupEmail.ariaValueMax,
+        signupBtnLoader.style.display = "block";
+        const {data,error} = await supabaseClient.auth.signUp({
+            email: signupEmail.value,
             password: signupPass.value
-        })
-        if(error) throw error
-        if(data) {
-            alert('please check your email for verification')
-        }
-        return data
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-async function login(){
-    try {
-        const {data, error} = await supabase.auth.signInWithPassword({
-            email: loginEmail.value,
-            password: loginPass.value
-        })
-
-        if(error) throw error
-        if(data){
-            console.log(data)
+        });
+        if (error) throw error;
+        if (data) {
+            console.log(data.user.id)
         }
     } catch (error) {
         
     }
 }
+
