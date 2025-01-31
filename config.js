fefab214a73271397b7ddfce1836d79a13bad27e
kamilahmed10121 <166241525+kamilahmed10121@users.noreply.github.com>
2025-01-31 20:42:15 +0500

    mai

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const {createClient} = supabase
+const supabaseUrl = 'https://jnlnxyrnetoetpfquhys.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpubG54eXJuZXRvZXRwZnF1aHlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxNjE0NjgsImV4cCI6MjA1MjczNzQ2OH0.hPljIm6YnIsJe3xVpzKtIacNFKs-iZMcbMEygJ_0A_w'
+const supabaseClient = createClient(supabaseUrl, supabaseKey)
 
-const supabaseUrl = 'https://jnlnxyrnetoetpfquhys.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpubG54eXJuZXRvZXRwZnF1aHlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxNjE0NjgsImV4cCI6MjA1MjczNzQ2OH0.hPljIm6YnIsJe3xVpzKtIacNFKs-iZMcbMEygJ_0A_w';
-
-const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
-window.supabase = supabase;
+window.supabase = supabaseClient
