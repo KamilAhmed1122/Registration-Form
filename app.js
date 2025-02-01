1bb38b077bfd9f58fb34b2cf57eb0e75319588af
KamilAhmed1122 <fatf13687@gmail.com>
2025-02-01 17:48:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-// async function checkSession() {
-//   try {
-//     const { data, error } = await supabase.auth.getSession();
-
-//     const authPages = ["/index.html", "/login.html", "/"];
-//     const currentPath = window.location.pathname;
-//     const isAuthPage = authPages.some((page) => page.includes(currentPath));
-
-//     const { session } = data;
-
-//     // // it will first check the session , if it's true then it will check Auth Page
-//     if(session) {
-//       if(isAuthPage) {
-//         window.location.href = '/dashboard.html'
-//     }
-//     } else {
-//         if(!isAuthPage) {
-//             window.location.href = '/login.html'
-//         }
-//     }
-
-//     // console.log(session);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// window.onload = checkSession;
-
 async function USERDETAILS() {
     console.log("getUserr ...");
     try {
